c465a890750ca21a7cc982018cb2563c8ae52982
johnnyjuarez <johnnyjuarez1992@gmail.com>
2020-10-02 14:55:30 -0500

    part component clean up

--- src/components/Parts/PartItemSelection.js
+++ src/components/Parts/PartItemSelection.js
@@ -1,15 +1,6 @@
 import React from "react";
 
 const PartItemSelection = (props) => {
-  // inputType="radio"
-  //           classNames="feature_option"
-  //           id={itemHash}
-  //           name={slugify(feature)}
-  //           checked={item.name === props.state.selected[feature].name}
-  //           onChange={(e) => props.updateFeature(feature, item)}
-  //           labelClassName="feature_label"
-  //           currenctyProp={props.currency.format(item.cost)}
-  //           itemName={item.name}
   return (
     <div>
       <input
